9f534db7cd1ba1d374561ee828069dcc84ee4286
Mojde7th <mojde7th@gmail.com>
2023-12-04 11:14:13 +0330

    document.write instead of console

--- js/main.js
+++ js/main.js
@@ -1,17 +1,41 @@
-debugger;
+//debugger;
 let x = +prompt("x...!");
 let y = +prompt("y...!");
 let operator = prompt("+,-,*,/,%,**");
 if (operator === "+") {
-  console.log(`result is ${x + y}`);
+  document.write(
+    `<h1 style="width: 30%; text-align: center; margin: 100px auto; background-color:#2b2b2b;color:white;padding:30px">result is ${
+      x + y
+    }</h1>`
+  );
 } else if (operator === "-") {
-  console.log(`result is ${x - y}`);
+  document.write(
+    `<h1 style="width: 30%; text-align: center; margin: 100px auto; background-color:#2b2b2b;color:white;padding:30px">result is ${
+      x - y
+    }</h1>`
+  );
 } else if (operator === "*") {
-  console.log(`result is ${x * y}`);
+  document.write(
+    `<h1 style="width: 30%; text-align: center; margin: 100px auto; background-color:#2b2b2b;color:white;padding:30px">result is ${
+      x * y
+    }</h1>`
+  );
 } else if (operator === "/") {
-  console.log(`result is ${x / y}`);
+  document.write(
+    `<h1 style="width: 30%; text-align: center; margin: 100px auto; background-color:#2b2b2b;color:white;padding:30px">result is ${
+      x / y
+    }</h1>`
+  );
 } else if (operator === "%") {
-  console.log(`result is ${x % y}`);
+  document.write(
+    `<h1 style="width: 30%; text-align: center; margin: 100px auto; background-color:#2b2b2b;color:white;padding:30px">result is ${
+      x % y
+    }</h1>`
+  );
 } else if (operator === "**") {
-  console.log(`result is ${x ** y}`);
+  document.write(
+    `<h1 style="width: 30%; text-align: center; margin: 100px auto; background-color:#2b2b2b;color:white;padding:30px">result is ${
+      x ** y
+    }</h1>`
+  );
 }
